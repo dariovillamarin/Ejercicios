9b6db5a8c27986081d8b66b0a7fcb0a66cf8cc4a
dariovillamarin <dario_villamarin@hotmail.com>
2024-03-06 21:56:11 -0500

    commit 1

---
+++ Ejercicio5Operadores.py
@@ -0,0 +1,22 @@
+# OPERADORES ARITMETICOS
+
+x=10
+y=5
+
+# SUMA
+print("La Suma de X + Y es: ",x+y)
+
+# RESTA
+print("La resta de X - Y es: ", x-y)
+
+# MULTIPLICACION
+print("La Multipliación de X * Y es: ", x*y)
+
+# DIVISION
+print("La división de X / Y es: ", x/y)
+
+# PRODUCTO ENTERO DE UNA DIVISION
+print("El producto entero de la división X // Y es: ", x//y)
+
+# EXPONENTE
+print ("El exponente de X ** Y es: ", x**5)
