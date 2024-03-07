27975e2e74db7aa9b3dd3ae8773f133e423828a0
dariovillamarin <dario_villamarin@hotmail.com>
2024-03-06 22:13:39 -0500

    commit 2

--- Ejercicio5Operadores.py
+++ Ejercicio5Operadores.py
@@ -20,3 +20,24 @@ print("El producto entero de la división X // Y es: ", x//y)
 
 # EXPONENTE
 print ("El exponente de X ** Y es: ", x**5)
+
+
+# OPERADORES COMPARATIVOS
+
+# MAYOR QUE
+print("X es mayor que Y ", x>y)
+
+# MENOR QUE
+print("X es menor que Y ", x<y)
+
+# IGUAL A
+print("X es igual a Y ", x==y)
+
+# NO ES IGUAL A
+print("X no es igual a Y ", x!=y)
+
+# MAYOR O IGUAL
+print("X es mayor o igual a Y ", x>=y)
+
+# MENOR O IGUAL
+print("X es menor o igual a Y", x<=y)
