e3a3340f0e020fe64d0157f5db46e09c69752a62
dariovillamarin <dario_villamarin@hotmail.com>
2024-04-08 21:11:59 -0500

    Commit 1

---
+++ Ejercicio7Match.py
@@ -0,0 +1,12 @@
+numero =1
+
+match(numero):
+    case 1:
+        print("Uno")
+    case 2:
+        print("Dos")
+    case 3:
+        print("Tres")
+    case _:
+        print("Número no reconocido")
+        
